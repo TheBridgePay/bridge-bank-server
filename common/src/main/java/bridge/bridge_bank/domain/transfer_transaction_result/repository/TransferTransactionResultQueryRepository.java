107b3a71738d3485b7d767c1ecd4207ed4fc5c89
khyun9807 <khyun9807@naver.com>
2026-03-23 21:30:11 +0900

    feat get10transfertransactionresults

--- common/src/main/java/bridge/bridge_bank/domain/transfer_transaction_result/repository/TransferTransactionResultQueryRepository.java
+++ common/src/main/java/bridge/bridge_bank/domain/transfer_transaction_result/repository/TransferTransactionResultQueryRepository.java
@@ -1,7 +1,10 @@
 package bridge.bridge_bank.domain.transfer_transaction_result.repository;
 
+import bridge.bridge_bank.domain.transfer_transaction_result.dto.TransferTransactionResultTargetOption;
 import bridge.bridge_bank.domain.transfer_transaction_result.entity.QTransferTransactionResult;
+import bridge.bridge_bank.domain.transfer_transaction_result.entity.TransferTransactionResult;
 import bridge.bridge_bank.domain.transfer_transaction_result.entity.TransferTransactionType;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -33,5 +37,32 @@ public class TransferTransactionResultQueryRepository {
                         qTransferTransactionResult.transferTransactionDate.lt(todayStart),
                         qTransferTransactionResult.transferTransactionType.eq(transferTransactionType)
                 ).fetchOne();
+    }
+
+    public List<TransferTransactionResult> get10TransferTransactionResults(
+            String senderAccountNumber,
+            TransferTransactionResultTargetOption option
+    ) {
+        return queryFactory
+                .selectFrom(qTransferTransactionResult)
+                .where(
+                        qTransferTransactionResult.selfAccountNumber.eq(senderAccountNumber),
+                        receiverAccountNumberEq(option.getReceiverAccountNumber()),
+                        transferTypeEq(option.getTransferTransactionType())
+                )
+                .orderBy(qTransferTransactionResult.transferTransactionDate.desc())
+                .limit(10)
+                .fetch();
+    }
+
+    private BooleanExpression receiverAccountNumberEq(String receiverAccountNumber) {
+        return receiverAccountNumber != null
+                ? qTransferTransactionResult.otherAccountNumber.eq(receiverAccountNumber)
+                : null;
+    }
+
+    private BooleanExpression transferTypeEq(TransferTransactionType type) {
+        return type != null ? qTransferTransactionResult.transferTransactionType.eq(type)
+                : null;
     }
 }
