1ffe228c05571bb30e0742fe231b1c23457d8c09
kyung hyun <131232309+khyun9807@users.noreply.github.com>
2026-03-23 18:57:37 +0900

    add swagger (#43)

--- core-server/src/main/java/bridge/bridge_bank/api/controller/TransferController.java
+++ core-server/src/main/java/bridge/bridge_bank/api/controller/TransferController.java
@@ -1,5 +1,6 @@
 package bridge.bridge_bank.api.controller;
 
+import bridge.bridge_bank.api.controller.docs.TransferControllerDocs;
 import bridge.bridge_bank.domain.transfer.TransferRequest;
 import bridge.bridge_bank.domain.transfer.TransferService;
 import jakarta.validation.Valid;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/transfers")
 @RequiredArgsConstructor
-public class TransferController {
+public class TransferController implements TransferControllerDocs {
 
     private final TransferService transferService;
 
     @PostMapping
+    @Override
     public ResponseEntity<Void> transfer(@Valid @RequestBody TransferRequest request) {
         transferService.simpleTransferNow(request);
         return ResponseEntity.ok().build();
